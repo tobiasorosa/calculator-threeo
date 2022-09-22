fb30df9e9817b97678955000878442b882d802f4
Tobias Rosa <tobias.rosa2000@gmail.com>
2022-09-22 12:48:57 -0300

    Add add route and calculator model

---
+++ backend/CalculatorThreeoAPI/CalculatorThreeoAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorThreeoAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorThreeoAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        Calc mCalc = new Calc();
+
+        [HttpGet]
+        [Route("add/{firstParam}/{secondParam}")]
+        public float Add(float firstParam, float secondParam)
+        {
+            return mCalc.Add(firstParam, secondParam);
+        }
+
+
+    }
+}
---
+++ backend/CalculatorThreeoAPI/CalculatorThreeoAPI/Models/Calc.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorThreeoAPI.Models
+{
+    public class Calc
+    {
+        public float Add(float firstValue, float secondValue)
+        {
+            return firstValue + secondValue;
+        }
+
+        public float Sub(float firstValue, float secondValue)
+        {
+            return firstValue - secondValue;
+        }
+
+        public float Mult(float firstValue, float secondValue)
+        {
+            return firstValue * secondValue;
+        }
+
+        public float Div(float firstValue, float secondValue)
+        {
+            return firstValue / secondValue;
+        }
+    }
+}
