516782c853a5b9d0cc8297fa823508522c02240c
Tobias Rosa <tobias.rosa2000@gmail.com>
2022-09-22 14:08:54 -0300

    Make api routes with calc

--- backend/CalculatorThreeoAPI/CalculatorThreeoAPI/Controllers/CalculatorController.cs
+++ backend/CalculatorThreeoAPI/CalculatorThreeoAPI/Controllers/CalculatorController.cs
@@ -1,5 +1,4 @@
 ﻿using CalculatorThreeoAPI.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CalculatorThreeoAPI.Controllers
@@ -8,15 +7,34 @@ namespace CalculatorThreeoAPI.Controllers
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
-        Calc mCalc = new Calc();
+        readonly Calc mCalc = new();
 
         [HttpGet]
         [Route("add/{firstParam}/{secondParam}")]
-        public float Add(float firstParam, float secondParam)
+        public float GetAdd(float firstParam, float secondParam)
         {
             return mCalc.Add(firstParam, secondParam);
         }
 
+        [HttpGet]
+        [Route("sub/{firstParam}/{secondParam}")]
+        public float GetSub(float firstParam, float secondParam)
+        {
+            return mCalc.Sub(firstParam, secondParam);
+        }
 
+        [HttpGet]
+        [Route("mult/{firstParam}/{secondParam}")]
+        public float GetMult(float firstParam, float secondParam)
+        {
+            return mCalc.Mult(firstParam, secondParam);
+        }
+
+        [HttpGet]
+        [Route("div/{firstParam}/{secondParam}")]
+        public float GetDiv(float firstParam, float secondParam)
+        {
+            return mCalc.Div(firstParam, secondParam);
+        }
     }
 }
